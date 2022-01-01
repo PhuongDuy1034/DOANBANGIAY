2b1f7d4548de93d63b9ed50c8b25fb93b69c81c2
PhuongDuy1034 <93701982+PhuongDuy1034@users.noreply.github.com>
2022-01-01 23:11:41 +0700

    add model

---
+++ database/migrations/2022_01_01_152204_create_accounts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('password');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('fullname');
+            $table->boolean('isadmin');
+            $table->string('avatar');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
