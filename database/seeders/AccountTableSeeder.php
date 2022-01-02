33f913a862d8d5352c3097fb80dfdeeb977e10e4
PhuongDuy1034 <93701982+PhuongDuy1034@users.noreply.github.com>
2022-01-03 02:49:26 +0700

    up view login

---
+++ database/seeders/AccountTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Account;
+use Faker\Factory as Faker;
+
+class AccountTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        Account::create([
+            'username' => 'VPDuy',
+            'email' => 'Pduy@gmail.com',
+            'password' => bcrypt('12345678'),
+            'phone' => '0327678818',
+            'address' => '45 Nguyễn Huệ',
+            'fullname' => 'Võ Phương Duy',
+            'isadmin' => '1',
+            'avatar' => 'Pduy',
+            'status' => '1',
+            ]);
+    }
+
+}
