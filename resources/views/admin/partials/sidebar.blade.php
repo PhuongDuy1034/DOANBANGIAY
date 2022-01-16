fb2542fd1cf37ae250006b277f26cde7da7ce033
PhuongDuy1034 <0306191307@caothang.edu.vn>
2022-01-17 02:09:00 +0700

    fix project

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -17,7 +17,7 @@
 		</form>
 		<ul class="nav menu">
 			<li class="active"><a href="index.html"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
-			<li><a href="widgets.html"><em class="fa fa-calendar">&nbsp;</em> Widgets</a></li>
+			<li><a href="{{ route('admin.account.index') }}"><em class="fa fa-calendar">&nbsp;</em> Account</a></li>
 			<li><a href="{{ route('admin.productType.index') }}"><em class="fa fa-bar-chart">&nbsp;</em> Manage ProductType</a></li>
 			<li><a href="elements.html"><em class="fa fa-toggle-off">&nbsp;</em> Manage Product</a></li>
 			<li><a href="{{ route('admin.provider.index') }}"><em class="fa fa-clone">&nbsp;</em> Manage Provider</a></li>
@@ -25,26 +25,7 @@
 			<li><a href="panels.html"><em class="fa fa-clone">&nbsp;</em> Manage Invoice</a></li>
 			<li><a href="panels.html"><em class="fa fa-clone">&nbsp;</em> Manage InvoiceDetail</a></li>
 
-			<li ><a href="index.html"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
-			<li><a href="{{ route('admin.account.index') }}"><em class="fa fa-calendar">&nbsp;</em> Account</a></li>
-			<li><a href="charts.html"><em class="fa fa-bar-chart">&nbsp;</em> Charts</a></li>
-			<li><a href="elements.html"><em class="fa fa-toggle-off">&nbsp;</em> UI Elements</a></li>
-			<li><a href="panels.html"><em class="fa fa-clone">&nbsp;</em> Alerts &amp; Panels</a></li>
-			<li class="parent "><a data-toggle="collapse" href="#sub-item-1">
-				<em class="fa fa-navicon">&nbsp;</em> Multilevel <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
-				</a>
-				<ul class="children collapse" id="sub-item-1">
-					<li><a class="" href="#">
-						<span class="fa fa-arrow-right">&nbsp;</span> Sub Item 1
-					</a></li>
-					<li><a class="" href="#">
-						<span class="fa fa-arrow-right">&nbsp;</span> Sub Item 2
-					</a></li>
-					<li><a class="" href="#">
-						<span class="fa fa-arrow-right">&nbsp;</span> Sub Item 3
-					</a></li>
-				</ul>
-			</li>
+			
 			<li><a href="{{ route('admin.logout')}}"><em class="fa fa-power-off">&nbsp;</em> Logout</a></li>
 		</ul>
 	</div>
--- routes/admin.php
+++ routes/admin.php
@@ -11,9 +11,7 @@ use Illuminate\Support\Arr;
   Route::get('register', [Admin\RegisterController::class, 'showRegisterForm'])->name('admin.register');
   Route::get('productType',[Admin\ProductTypeController::class,'index'])->name('admin.productType.index');
   Route::get('provider',[Admin\ProvidersController::class,'index'])->name('admin.provider.index');
-Route::get('/', function () {
-return view('admin.dashboard.index');
-});
+
 
   Route::get('Account', [Admin\AccountController::class, 'index'])->name('admin.account.index');
    Route::post('Account', [Admin\AccountController::class, 'create'])->name('admin.account.create');
