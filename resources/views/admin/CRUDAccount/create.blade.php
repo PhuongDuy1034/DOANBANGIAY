f050fd2bb53d09177861dd85c60664e13c72f821
PhuongDuy1034 <0306191307@caothang.edu.vn>
2022-02-16 08:55:35 +0700

    add,view,edit,delete Account

--- resources/views/admin/CRUDAccount/create.blade.php
+++ resources/views/admin/CRUDAccount/create.blade.php
@@ -5,15 +5,27 @@
   <div class="row">
       <div class="col-md-9">
           <div class="card">
-              <div class="card-header"><h2>Create</h2></div>
+              <div class="card-header"><h2>Create Account</h2></div>
               <div class="card-body">
                   
-                  <form action="{{ url('Account') }}" method="post">
-                    {!! csrf_field() !!}
+                  <form action="{{ route('admin.account.store') }}" method="post">
+                    @csrf
                     <label>Name</label></br>
                     <input type="text" name="username" id="username" class="form-control"></br>
+                    <label>Password</label></br>
+                    <input type="text" name="password" id="password" class="form-control"></br>
+                    <label>Email</label></br>
+                    <input type="text" name="email" id="email" class="form-control"></br>
+                    <label>Fullname</label></br>
+                    <input type="text" name="fullname" id="fullname" class="form-control"></br>
+                    <label>Isadmin</label></br>
+                    <input type="text" name="isadmin" id="isadmin" class="form-control"></br>
+                    <label>Status</label></br>
+                    <input type="text" name="status" id="status" class="form-control"></br>
                     <label>Address</label></br>
                     <input type="text" name="address" id="address" class="form-control"></br>
+                    <label>Avata</label></br>
+                    <input type="text" name="avatar" id="avatar" class="form-control"></br>
                     <label>Phone</label></br>
                     <input type="text" name="phone" id="phone" class="form-control"></br>
                     <input type="submit" value="Save" class="btn btn-success"></br>
